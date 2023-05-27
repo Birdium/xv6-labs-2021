17bc5715df2108d660528f87c2a4b7e045e1b60b
Birdium <bibishu2001@163.com>
2023-05-27 15:34:11 +0800

    init & mem alloc

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,14 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
-kinit()
+kinit() 
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; i++) {
+    initlock(&kmem[i].lock, "kmem");
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -43,6 +45,9 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
+
+#define cur_kmem kmem[cpuid()]
+
 void
 kfree(void *pa)
 {
@@ -56,10 +61,14 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+
+  acquire(&cur_kmem.lock);
+  r->next = cur_kmem.freelist;
+  cur_kmem.freelist = r;
+  release(&cur_kmem.lock);
+
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +79,30 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+
+  acquire(&cur_kmem.lock);
+  r = cur_kmem.freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    cur_kmem.freelist = r->next;
+  release(&cur_kmem.lock);
+
+  if (!r) {
+    for (int i = 0; i < NCPU; i++) {
+      if (i != cpuid()) {
+        acquire(&kmem[i].lock);
+        if (kmem[i].freelist) {
+          r = kmem[i].freelist;
+          kmem[i].freelist = r->next;
+          release(&kmem[i].lock);
+          break;
+        }
+        release(&kmem[i].lock); 
+      }
+    }
+  }
+
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
